7bee9e9a7d38071f8d8be880c248877998499ee4
popi753 <luka.popi.919@gmail.com>
2024-02-02 11:02:24 +0400

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import "../styles/home.css";
+import { useNavigate } from "react-router-dom";
+
+
+export default  function Home(){
+    async function fetchData () {
+        try {
+            const response = await fetch(process.env.REACT_APP_API+"getteams");
+            const teams = await response.json();
+            // console.log(teams)
+            setList(teams)
+        } catch (error) {
+            window.alert(error+"/try later")
+            console.error(error)
+        }
+    }
+
+    const navigate = useNavigate()
+
+    const [list,setList] = useState()
+    
+    useEffect(()=>{
+
+
+        if(window.localStorage.getItem("person") == null){
+            fetchData()
+        }else{
+            navigate("/ranking")
+
+        }
+    },[])
+
+
+    
+
+
+  
+    
+
+    
+
+
+    return(
+        <div className="Home">
+            <p className="p"> აირჩიე შენი გუნდი </p>
+            <div className="teams">
+
+                {list && list.map((team,index)=>{
+                    return(
+                            <div className="team" key={index} 
+                            onClick={async()=>{
+                                if (team.id == 47) {
+                                    team.manager = "GOD"
+                                }
+                                
+                                window.localStorage.setItem("person", JSON.stringify(team))
+                                               navigate("/ranking")}}>
+                                <img src={team.logo} title={`${team.name}/${team.manager}`} alt="logo"/>
+                                <p   
+                                style={{
+                                    "color":
+                                    // "#F9A620"
+                                    "#138A36"
+                                }}
+                                >{team.name}</p>
+                                <p   
+                                style={{
+                                    "color":
+                                    // "#FF3A20"
+                                    "#04E824"
+                                }}
+                                >{team.manager}</p>
+                            </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/YourRanking.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+// import "../styles/yourRanking.css";
+
+
+export default function YourRanking() {
+
+    const navigate = useNavigate()
+
+    const [list,setList] = useState()
+
+    const userTeam = JSON.parse(window.localStorage.getItem("person"))
+
+async function fetchRanking() {
+
+    try {
+        const response = await fetch(process.env.REACT_APP_API+"getranking", {
+          headers: {
+              'Content-Type': 'application/json',
+              "person": window.localStorage.getItem("person")
+
+            },
+          },
+          )
+          const data = await response.json() 
+          setList(data)
+        //   console.log(data)
+        
+      } catch (error) {
+        window.alert(error+"/try later")
+        console.error(error);
+      }
+}
+
+useEffect(()=>{
+    if (!window.localStorage.getItem("person")) {
+        navigate("/")
+        return
+    }
+    try {
+        JSON.parse(window.localStorage.getItem("person"))
+        console.log(true)
+    } catch (error) {
+        console.log(false)
+        window.localStorage.removeItem("person")
+        navigate("/")
+        return
+    }
+    fetchRanking()
+},[])
+
+    return(
+
+    <>
+
+
+<div className="box" style={{"paddingBottom":"25px"}}>
+    {userTeam && 
+    <div className="userTeam">
+    <img src={userTeam.logo} title={userTeam.name} alt="logo"/>
+    <div>
+        <p>{userTeam.name}</p> <br />
+        <p>{userTeam.manager}</p>
+    </div>
+
+    <button className="logout"
+    onClick={()=>{
+        window.localStorage.removeItem("person")
+        window.location.href = "/";
+
+    }}>logout</button>
+</div>}
+    
+    
+    
+                <div >
+                        <p
+                        style={{
+                            "fontSize": "25px",
+    "fontWeight": "500",
+    "margin": "50px 0 25px 25px"
+                        }}
+                        >შენი არჩეული ცხრილი :</p>
+                    <div className="container">
+                        <div className="ranking">
+                                        {list && list.map((element) => {
+                                            return (
+                                                <ChosenGroup
+                                                key={element.rank}
+                                                team={element}
+                                                />
+                                            );
+                                        })}
+                        </div>
+    
+                    </div>
+    
+    
+    
+                </div>
+</div> 
+    </>
+    )
+
+}
+
+
+export function ChosenGroup({ handleClick, handleDelete, team, deleteable }) {
+    return (
+        <div className="ranking-team">
+            <span >{team.rank}.</span>
+            
+            <img
+                style={
+                    !team.logo ? { "filter":"invert(100%" } : {}
+                    
+                }
+                src={
+                    team.logo || "https://cdn-icons-png.flaticon.com/512/5042/5042057.png"
+                }
+                height="50px"
+                alt="club logo"
+            />
+            <span
+                
+            >
+                {team.name ? `${team.name}/${team.manager}` : "აირჩიე კლუბი"}
+            </span>
+            
+            
+        </div>
+    );
+}
